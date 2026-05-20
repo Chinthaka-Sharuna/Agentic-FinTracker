8169d6e0e8f2c234ef7479c91ed2137433a705aa
Chinthaka-Sharuna <sharunadrc@gmail.com>
2026-05-20 20:44:08 +0530

    feat: Remove unused tool configurations from Config class to streamline code

--- app/config/config.py
+++ app/config/config.py
@@ -27,17 +27,3 @@ class Config:
     PDF_EXTRACTOR_MODEL = _config["pdf_extractor"]["model"]
     PDF_EXTRACTOR_SYSTEM_PROMPT = _config["pdf_extractor"]["system_prompt"]
     PDF_EXTRACTOR_BASE_URL = _config["pdf_extractor"]["base_url"]
-
-    # Tool configurations from YAML
-    TOOLS_NAMES = _config["tools"]["names"]
-
-    SET_SALARY_DESC = _config["tools"]["set_salary"]["description"]
-    SET_SALARY_PROPS = _config["tools"]["set_salary"]["properties"]
-
-    LOG_EXPENSE_DESC = _config["tools"]["log_expense"]["description"]
-    LOG_EXPENSE_PROPS = _config["tools"]["log_expense"]["properties"]
-
-    GET_BALANCE_DESC = _config["tools"]["get_balance"]["description"]
-
-    GET_EXPENSE_SUMMARY_DESC = _config["tools"]["get_expense_summary"]["description"]
-    GET_EXPENSE_SUMMARY_PROPS = _config["tools"]["get_expense_summary"]["properties"]
